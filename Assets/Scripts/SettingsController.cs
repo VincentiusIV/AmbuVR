8c2c9f4623142adddda8d21be371e65fc1e0e280
Vincent Versnel <vincent@versnelhifi.nl>
2017-03-05 17:51:45 +0100

    Created SettingsController

---
+++ Assets/Scripts/SettingsController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsController : MonoBehaviour {
+
+    [SerializeField]GameObject player;
+    [SerializeField]GameObject playerVR;
+    [SerializeField]Transform spawnPos;
+
+    public bool enableVR;
+
+    void Start()
+    {
+        if (enableVR)
+            Instantiate(playerVR, spawnPos.position, Quaternion.identity);
+        else Instantiate(player, spawnPos.position, Quaternion.identity);
+    }
+}
