35e31b8d1ee1f679a260b4ca28d76b06b9253dbf
Vincent Versnel <vincent@versnelhifi.nl>
2017-03-10 14:42:22 +0100

    Picking up & throwing physics

--- Assets/Scripts/ViveController.cs
+++ Assets/Scripts/ViveController.cs
@@ -42,6 +42,28 @@ public class ViveController : MonoBehaviour
             Debug.Log("touching the application menu");
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("VR_Controller"))
+            return;
+        else
+        {
+            if(device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                // pickup
+                other.attachedRigidbody.isKinematic = true;
+                //other.transform.position = transform.position;
+                other.transform.SetParent(transform);
+            }
+            if(device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                // let go
+                other.attachedRigidbody.isKinematic = false;
+                other.transform.SetParent(null);
+                ThrowVelocity(other.attachedRigidbody);
+            }
+        }
+    }
     // Pointer
     private void DrawPointer()
     {
@@ -58,6 +80,21 @@ public class ViveController : MonoBehaviour
         else
         {
             pointer.SetPosition(1, pointerOrigin.position + (pointerOrigin.forward * pointerLength));
+        }
+    }
+
+    private void ThrowVelocity(Rigidbody rb)
+    {
+        Transform origin = motionCon.origin ? motionCon.origin : motionCon.transform.parent;
+        if (origin != null)
+        {
+            rb.velocity = origin.TransformVector(device.velocity);
+            rb.angularVelocity = origin.TransformVector(device.angularVelocity);
+        }
+        else
+        {
+            rb.velocity = device.velocity;
+            rb.angularVelocity = device.angularVelocity;
         }
     }
 }
