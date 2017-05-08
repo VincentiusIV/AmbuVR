fd846c2f6d3677467ddbfd4e11bcb2d69c540c4a
Vincent Versnel <vincent@versnelhifi.nl>
2017-05-08 10:55:03 +0200

    Fixed some more version bugs, added object spawner for easier testing

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    [Header("Mandatory References")]
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private Transform objectSpawnPos;
+    [SerializeField] private TextMesh text;
+
+    private void Start()
+    {
+        text.text = objectToSpawn.name;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("VR_Controller"))
+        {
+            Instantiate(objectToSpawn, objectSpawnPos.position, Quaternion.identity);
+        }
+    }
+}
